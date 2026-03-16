ef3772d751379e8bb65b3f55ea22ed870589de92
Tumisang(Romio) Lesimola <romeopomeo1@gmail.com>
2026-03-17 00:50:02 +0200

    Assignment Complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,54 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fitness App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.lightGreenAccent),
       ),
-      home: const MyHomePage(title: 'Fitness & Workout App'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({this.optionalMessage, super.key});
+
+  static const String appName = "Fitness & Tracker Tracker";
+  final String? optionalMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.lightGreenAccent, Colors.lightBlueAccent]),
+          ),
+        ),
+        title: const Text(appName),
+        leading: const Icon(Icons.fitness_center),
+        actions: [
+          IconButton(onPressed: () {}, 
+          icon: Icon(Icons.search),
+          ),
+          IconButton(onPressed: () {}, 
+          icon: Icon(Icons.person),
+          )
+        ],
+      ),
+
+      body: Center(
+        child: Text(optionalMessage ?? 'Welcome to your Fitness & Workout App...'),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+        ),
     );
   }
 }
